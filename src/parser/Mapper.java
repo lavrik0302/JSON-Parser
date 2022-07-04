d2450b248cd575784fb0ba2565882be95aefb815
alexey.lavrenchuk <alexey.lavrenchuk@intexsoft.by>
2022-07-04 14:11:20 +0300

    Fixing mapping for Arrays

--- src/parser/Mapper.java
+++ src/parser/Mapper.java
@@ -5,34 +5,39 @@ import model.*;
 import java.util.*;
 import java.util.List;
 
+
 public class Mapper {
-    public static <T> Object map(JsonNode jsonNode, Class<T> classType) {
-        if (classType.isAssignableFrom(Boolean.class)||classType.isAssignableFrom(boolean.class)) {
-            return ((JsonBoolean) jsonNode).getJsonBoolean();
+
+    public static <T> T map(JsonNode jsonNode, Class<T> classType) {
+        if (classType.isAssignableFrom(Boolean.class) || classType.isAssignableFrom(boolean.class)) {
+            return (T) (Object) (((JsonBoolean) jsonNode).getJsonBoolean());
         } else if (classType.isAssignableFrom(String.class)) {
-            return ((JsonString) jsonNode).getJsonString();
+            return (T) ((JsonString) jsonNode).getJsonString();
         } else if (Number.class.isAssignableFrom(classType)) {
-            return ((JsonNumber) jsonNode).getJsonNumber();
+            return (T) ((JsonNumber) jsonNode).getJsonNumber();
         } else if (Collection.class.isAssignableFrom(classType)) {
             List<JsonNode> jsonNodes = new ArrayList<>(((JsonArray) jsonNode).getValues());
-            return jsonNodes;
-        } else if (classType.isAssignableFrom(Arrays.class)) {
-                          Object[] array = ((JsonArray) jsonNode).getValues().toArray(new Object[0]);
-                          for(int i=0;i<array.length;i++){
-                              if (array[i].getClass().isAssignableFrom(JsonBoolean.class)) {
-                                  array[i] = map((JsonNode) array[i],Boolean.class );
-                              } else if (array[i].getClass().isAssignableFrom(JsonString.class)) {
-                                  array[i] = map((JsonNode) array[i],String.class );
-                              } else if
-                              (array[i].getClass().isAssignableFrom(JsonNumber.class)) {
-                                  array[i] = map((JsonNode) array[i],Number.class );
-                              } else if (Collection.class.isAssignableFrom(array[i].getClass())) {
-                                  array[i] = map((JsonNode) array[i],Collection.class);
-                              }else if(array[i].getClass().isAssignableFrom(Arrays.class)) {
-                                  array[i] = map((JsonNode) array[i],Arrays.class );
-                              }else array[i]=null;
-                          }
-                          return array;
+            return (T) jsonNodes;
+        } else if (classType.isArray()) {
+            List list = ((JsonArray) jsonNode).values;
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).getClass().isAssignableFrom(JsonBoolean.class)) {
+                    list.set(i, map((JsonNode) list.get(i), Boolean.class));
+                } else if (list.get(i).getClass().isAssignableFrom(JsonString.class)) {
+                    list.set(i, map((JsonNode) list.get(i), String.class));
+                } else if
+                (list.get(i).getClass().isAssignableFrom(JsonNumber.class)) {
+                    System.out.println(1);
+                    list.set(i, map((JsonNode) list.get(i), Number.class));
+                } else if (Collection.class.isAssignableFrom(list.get(i).getClass())) {
+                    list.set(i, map((JsonNode) list.get(i), Collection.class));
+                } else if (list.get(i).getClass().isAssignableFrom(Arrays.class)) {
+                    list.set(i, map((JsonNode) list.get(i), Arrays.class));
+                } else list.set(i, null);
+            }
+            T array = (T) list.toArray();
+            System.out.println(array.getClass());
+            return array;
         }
         return null;
     }
@@ -40,20 +45,34 @@ public class Mapper {
     public static void main(String[] args) {
         JsonBoolean jsonNode1 = new JsonBoolean();
         jsonNode1.setJsonBoolean(true);
+        System.out.println(jsonNode1);
         JsonString jsonNode2 = new JsonString();
         jsonNode2.setJsonString("some text for test");
-
+        System.out.println(jsonNode2);
         JsonNumber jsonNode3 = new JsonNumber();
-        jsonNode3.setJsonNumber((int)12.12);
-
+        jsonNode3.setJsonNumber((int) 12.12);
+        System.out.println(jsonNode3);
         JsonArray jsonNode4 = new JsonArray();
-        jsonNode4.add(0, jsonNode1);
-        jsonNode4.add(1, jsonNode2);
+        jsonNode4.add(0, jsonNode3);
+        jsonNode4.add(1, jsonNode3);
         jsonNode4.add(2, jsonNode3);
-        Object arr [] =  (Object[]) map(jsonNode4, Arrays.class);
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
+        System.out.println(jsonNode4.values);
+        JsonArray jsonNode5 = new JsonArray();
+        jsonNode5.add(0, jsonNode3);
+        jsonNode5.add(0, jsonNode3);
+        jsonNode5.add(0, jsonNode3);
+        int[] arr = map(jsonNode5, int[].class);
+        System.out.println(arr.getClass());
+        int[] arr1 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr1[i] = (int) arr[i];
+        }
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+        System.out.println(arr1[2]);
+       // System.out.println(arr[0].getClass());
+      //  System.out.println(arr[1].getClass());
+       // System.out.println(arr[2].getClass());
         System.out.println();
     }
 
