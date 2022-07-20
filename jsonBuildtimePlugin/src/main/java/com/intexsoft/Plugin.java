734a784d90f5f57d3920b7602a44d0acd82f16de
alexey.lavrenchuk <alexey.lavrenchuk@intexsoft.by>
2022-07-20 14:04:22 +0300

    fixed plugin

--- jsonBuildtimePlugin/src/main/java/com/intexsoft/Plugin.java
+++ jsonBuildtimePlugin/src/main/java/com/intexsoft/Plugin.java
@@ -39,8 +39,15 @@ public class Plugin extends AbstractMojo {
                 }
                 JsonNode jsonNode = JsonDeserializer.parse(stringBuilder.toString());
                 if (jsonNode.getClass().isAssignableFrom(JsonObject.class)) {
-                    File jsonObjectFile = new File("buildtime/src/main/java/com/intexsoft/" + tempFile.getName().substring(0, 1).toUpperCase() + tempFile.getName().substring(1, tempFile.getName().indexOf('.')) + ".java");
-                    jsonObjectFile.createNewFile();
+                    System.out.println("here");
+                    File dirTar=new File("buildtime/target/");
+                    dirTar.mkdir();
+                    File dir=new File("buildtime/target/generated-sources/");
+                    dir.mkdir();
+                    System.out.println(dir);
+                    File jsonObjectFile = new File("buildtime/target/generated-sources/" + tempFile.getName().substring(0, 1).toUpperCase() + tempFile.getName().substring(1, tempFile.getName().indexOf('.')) + ".java");
+                    System.out.println(jsonObjectFile); jsonObjectFile.createNewFile();
+                    System.out.println("aftercreatenew file");
                     mapObject(jsonNode, jsonObjectFile);
                 }
             } catch (IOException e) {
@@ -52,12 +59,13 @@ public class Plugin extends AbstractMojo {
     public void mapObject(JsonNode jsonNode, File jsonObjectFile) throws IOException {
         FileWriter fileWriter = new FileWriter(jsonObjectFile, false);
         String fieldNames[] = ((JsonObject) jsonNode).values.keySet().toArray(new String[0]);
-        fileWriter.write("package com.intexsoft;\nimport lombok.*;\n\n@Data\npublic class " + jsonObjectFile.getName().substring(0, 1).toUpperCase() + jsonObjectFile.getName().substring(1, jsonObjectFile.getName().indexOf('.')) + " { ");
+        fileWriter.write("\nimport lombok.*;\n\n@Data\npublic class " + jsonObjectFile.getName().substring(0, 1).toUpperCase() + jsonObjectFile.getName().substring(1, jsonObjectFile.getName().indexOf('.')) + " { ");
         for (String fieldName : fieldNames) {
             if (!((JsonObject) jsonNode).get(fieldName).getClass().isAssignableFrom(JsonObject.class)) {
                 fileWriter.write("\n    private " + ((JsonObject) jsonNode).get(fieldName).getClass().getSimpleName().replace("Json", "") + " " + fieldName + " = " + ((JsonObject) jsonNode).get(fieldName) + ";\n");
             } else {
-                File file = new File("buildtime/src/main/java/com/intexsoft/" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + ".java");
+                System.out.println("sdas");
+                File file = new File("buildtime/target/generated-sources/" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + ".java");
                 file.createNewFile();
                 mapObject(((JsonObject) jsonNode).get(fieldName), file);
                 fileWriter.write("\n    private " + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + " " + fieldName + " = new " + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + "().get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + "();\n");
